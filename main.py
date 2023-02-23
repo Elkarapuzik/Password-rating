0a92b170f8a30d14d2fcac1a3baa702eac149aae
Elkarapuzik <shmakov-10@bk.ru>
2023-02-23 10:40:25 +0300

    Удалил не нужный main

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
 def has_symbols(password):
-    mimi = False
-    for simvol in password:
-        if not simvol.isdigit() and not simvol.isalpha():
-            mimi = True
-    return mimi 
+    return True in [not simbol.isdigit() and not simbol.isalpha() for simbol in password ]
 
 def has_digit(password):
-    mimi = False
-    for simvol in password:
-        if simvol.isdigit():
-            mimi = True
-    return mimi
+    return True in [simbol.isdigit() for simbol in password ]
 
 def has_letters(password):
-    mimi = False
-    for simvol in password:
-        if simvol.isalpha():
-            mimi = True
-    return mimi
+    return True in [simbol.isalpha() for simbol in password ]
 
 def has_upper_letters(password):
-    mimi = False
-    for simvol in password:
-        if simvol.isupper():
-            mimi = True
-    return mimi
+    return True in [simbol.isupper() for simbol in password ]
 
 def has_lower_letters(password):
-    mimi = False
-    for simvol in password:
-        if simvol.islower():
-            mimi = True
-    return mimi
+    return True in [simbol.islower() for simbol in password ]
 
 def is_very_long(password):
     if (len(password)) > 11:
@@ -39,15 +19,15 @@ def is_very_long(password):
     else:
         return False
 
-password = input("Введите пароль: ")
+if __name__ == '__main__':
+    password = input("Введите пароль: ")
 
+    password_check = [  
+    has_digit , is_very_long , has_letters , has_lower_letters , has_upper_letters, has_symbols]
 
-password_check = [
-has_digit , is_very_long , has_letters , has_lower_letters , has_upper_letters, has_symbols]
+    score = 0
+    for check in password_check :
+        if check(password):
+            score += 2
 
-score = 0
-for check in password_check :
-    if check(password):
-        score += 2
-
-print (f"Рейтинг пароля: {score}")
+    print (f"Рейтинг пароля: {score}")
